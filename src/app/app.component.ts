5390b1d50a35b6717231d9a173f86f1e279b4010
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-05-02 16:36:58 -0500

    chore: global style variables configured. Toggle theme service created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ThemeService } from './core/services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    MatButtonModule, 
+    MatCardModule, 
+    MatIconModule,
+    MatTooltipModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'swiftport-frontend';
+  isDarkTheme = false;
+
+  constructor(private themeService: ThemeService) {
+    this.themeService.theme$.subscribe(theme => {
+      this.isDarkTheme = theme === 'dark';
+    });
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
 }
