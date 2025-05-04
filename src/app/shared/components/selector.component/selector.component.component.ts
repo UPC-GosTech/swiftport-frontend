26764a37281b091b462aec848f1e70456e32eaee
natalia <natalia.roman.cruz@gmail.com>
2025-05-04 00:19:08 -0500

    chore: modify selector.component.ts

--- src/app/shared/components/selector.component/selector.component.component.ts
+++ src/app/shared/components/selector.component/selector.component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-selector.component',
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './selector.component.component.scss'
 })
 export class SelectorComponentComponent {
+  @Input() options: string[] = [];
+  @Input() color: 'secondary' | 'seccess' | 'error' | 'warning' = 'secondary';
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
 
+  @Output() selectionChange = new EventEmitter<string>();
+
+  selectedOption: string = '';
+
+  onSelectionChange(option: string) {
+    this.selectedOption = option;
+    this.selectionChange.emit(this.selectedOption);
+  }
 }
