15e5defaf85ff9132145e490f3e268ae6b771816
natalia <natalia.roman.cruz@gmail.com>
2025-05-04 16:48:38 -0500

    feat(in-app-navigation): setup in-app navigation rules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { ComponentsDemoComponent } from './shared/views/components-demo/components-demo.component';
-export const routes: Routes = [
-    {
-        path: 'demo',
-        component: ComponentsDemoComponent
-    }
-];
+
+export const routes: Routes = [];
