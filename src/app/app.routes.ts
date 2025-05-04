836aa12ee95ee3f1734a8a4f4e8a704c827f5d64
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-05-04 17:06:40 -0500

    feat: table component adapted and implemented. Demo route added for testing components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ComponentsDemoComponent } from './shared/views/components-demo/components-demo.component';
+export const routes: Routes = [
+    {
+        path: 'demo',
+        component: ComponentsDemoComponent
+    }
+];
