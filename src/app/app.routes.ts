272f71e90d983587bb7c7cbaeb8849158ddb19ce
natalia <natalia.roman.cruz@gmail.com>
2025-05-04 16:48:38 -0500

    feat(in-app-navigation): setup in-app navigation rules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
+import {RegisterComponent} from './core/registration/views/register/register.component';
+import {AccountCreationComponent} from './core/registration/views/account-creation/account-creation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register',         component: RegisterComponent },
+  { path: 'account',          component: AccountCreationComponent},
+  { path: '',                 redirectTo: 'register', pathMatch: 'full' },
+  { path: '**',               component: PageNotFoundComponent }
+];
