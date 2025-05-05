036a7db8cb4556332494dfdc85e5e0558570a48e
natalia <natalia.roman.cruz@gmail.com>
2025-05-04 22:51:23 -0500

    chore: Modidy payment information html and ts

--- src/app/core/registration/views/payment-information/payment-information.component.ts
+++ src/app/core/registration/views/payment-information/payment-information.component.ts
@@ -1,15 +1,78 @@
 import { Component } from '@angular/core';
 import {InputComponent} from '../../../../shared/components/input/input.component';
+import {SelectorComponent} from '../../../../shared/components/selector/selector.component';
+import {ButtonComponent} from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-payment-information',
   imports: [
-    InputComponent
+    InputComponent,
+    SelectorComponent,
+    ButtonComponent
   ],
   templateUrl: './payment-information.component.html',
   styleUrl: './payment-information.component.scss'
 })
 export class PaymentInformationComponent {
   name: string = '';
-  placeholderName: string = 'Ingrese su email...';
+  placeholderName: string = 'Nombre completo';
+  company: string = '';
+  placeholderCompany: string = 'Nombre completo';
+  cardnumber: string = '';
+  placeholderCardnumber: string = 'Numero de tarjeta';
+  ruc: string = '';
+  placeholderRuc: string = 'Numero de RUC';
+  cvv: string = '';
+  placeholderCvv: string = 'CVV';
+
+  cardType: string = '';
+  cardTypeList = ['Crédito', 'Débito'];
+  month: string = '';
+  monthList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+  year: string = '';
+  yearList = ['2025', '2026', '2027', '2028', '2029', '2030', '2031', '2032', '2033', '2034', '2035', '2036'];
+
+  onNameChange(option: string) {
+    this.name = option;
+    console.log(option);
+  }
+  onCompanyChange(option: string) {
+    this.company = option;
+    console.log(option);
+  }
+  onCardNumberChange(option: string) {
+    this.cardnumber = option;
+    console.log(option);
+  }
+  onRucChange(option: string) {
+    this.ruc = option;
+    console.log(option);
+  }
+  onCvvChange(option: string) {
+    this.cvv = option;
+    console.log(option);
+  }
+  onCardTypeChange(option: string) {
+    this.cardType = option;
+    console.log(option);
+  }
+  onMonthChange(option: string) {
+    this.month = option;
+    console.log(option);
+  }
+  onYearChange(option: string) {
+    this.year = option;
+    console.log(option);
+  }
+
+  onSaveInformation() {
+    this.onNameChange(this.name);
+    this.onCompanyChange(this.company);
+    this.onCardNumberChange(this.cardnumber);
+    this.onRucChange(this.ruc);
+    this.onCvvChange(this.cvv);
+    this.onCardTypeChange(this.cardType);
+    this.onMonthChange(this.month);
+    this.onYearChange(this.year);
+  }
 }
