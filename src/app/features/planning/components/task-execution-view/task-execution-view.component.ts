fc56620b312f7697a37fe904e4fc1fb2d07b897d
natalia <natalia.roman.cruz@gmail.com>
2025-05-06 20:37:08 -0500

    feat: Add time field

--- src/app/features/planning/components/task-execution-view/task-execution-view.component.ts
+++ src/app/features/planning/components/task-execution-view/task-execution-view.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {InputComponent} from "../../../../shared/components/input/input.component";
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-task-execution-view',
-  imports: [],
+  imports: [
+    InputComponent,
+    FormsModule
+  ],
   templateUrl: './task-execution-view.component.html',
   styleUrl: './task-execution-view.component.scss'
 })
 export class TaskExecutionViewComponent {
+  startTime: string = '';
+  startTimeDisabled: boolean = false;
 
+  onStartTimeChange(value: string): void {
+    this.startTime = value;
+    console.log('Start time: ', this.startTime);
+  }
 }
