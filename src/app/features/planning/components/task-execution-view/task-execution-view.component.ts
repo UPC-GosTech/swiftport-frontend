1b1f7f37cec22dd690a8e5699e342ea0002def13
natalia <natalia.roman.cruz@gmail.com>
2025-05-06 20:00:36 -0500

    feat: Add task-execution-view component

---
+++ src/app/features/planning/components/task-execution-view/task-execution-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-execution-view',
+  imports: [],
+  templateUrl: './task-execution-view.component.html',
+  styleUrl: './task-execution-view.component.scss'
+})
+export class TaskExecutionViewComponent {
+
+}
