5c2a33c88b3befbb235966ca1b43238d00d89338
natalia <natalia.roman.cruz@gmail.com>
2025-05-06 23:31:03 -0500

    feat: Add task-scheduling-dialog component

---
+++ src/app/features/planning/components/task-scheduling-dialog/task-scheduling-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-scheduling-dialog',
+  imports: [],
+  templateUrl: './task-scheduling-dialog.component.html',
+  styleUrl: './task-scheduling-dialog.component.scss'
+})
+export class TaskSchedulingDialogComponent {
+
+}
