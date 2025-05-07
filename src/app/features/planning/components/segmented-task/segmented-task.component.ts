e90e63ecff0fdf0501e0435cde2579a4dde369e5
natalia <natalia.roman.cruz@gmail.com>
2025-05-07 09:16:04 -0500

    feat: Add segmented-task component

---
+++ src/app/features/planning/components/segmented-task/segmented-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segmented-task',
+  imports: [],
+  templateUrl: './segmented-task.component.html',
+  styleUrl: './segmented-task.component.scss'
+})
+export class SegmentedTaskComponent {
+
+}
