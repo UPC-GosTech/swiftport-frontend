3c23919a82eabf40a0f7f71615ea0b07830b31c8
natalia <natalia.roman.cruz@gmail.com>
2025-05-07 22:32:16 -0500

    feat: Add activity-detail-view component

---
+++ src/app/features/planning/components/activity-detail-view/activity-detail-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-detail-view',
+  imports: [],
+  templateUrl: './activity-detail-view.component.html',
+  styleUrl: './activity-detail-view.component.scss'
+})
+export class ActivityDetailViewComponent {
+
+}
