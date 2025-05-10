c933962dc993520f83c86692fe5cdea4a2868f30
Arturo Adrianzén <cartureto@gmail.com>
2025-05-10 18:51:17 -0500

    feat: Update input component for form control

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,24 +1,57 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component, EventEmitter, forwardRef, Input, Output} from '@angular/core';
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [
-    FormsModule
-  ],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrls: ['./input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputComponent),
+    multi: true
+  }]
 })
-export class InputComponent {
-  @Input() value: string = '';
+export class InputComponent implements ControlValueAccessor {
   @Input() placeholder: string = '';
   @Input() disabled: boolean = false;
   @Input() type: string = 'text';
+  @Input() maxLength!: number;
 
   @Output() valueChange = new EventEmitter<string>();
 
+  @Input() value = '';
+
+  private onChange = (_: any) => {};
+  private onTouched = () => {};
+
+  // Llamado por Angular cuando el FormControl cambia externamente
+  writeValue(obj: any): void {
+    this.value = obj ?? '';
+  }
+  // Angular nos da la función que debemos llamar cuando el valor interno cambia
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  // Angular nos da la función que debemos llamar cuando se toca (blur)
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  // Cuando el FormControl se deshabilita/habilita
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  // Al cambiar el input
   onInputChange(value: string) {
     this.value = value;
     this.valueChange.emit(this.value);
+    this.onChange(this.value);
+  }
+
+  // Al perder foco
+  onBlur() {
+    this.onTouched();
   }
 }
