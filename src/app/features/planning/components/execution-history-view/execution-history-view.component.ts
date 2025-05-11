b90ec574fe9f9c2e78f734b3aff0685b1b6fe6bc
natalia <natalia.roman.cruz@gmail.com>
2025-05-10 22:54:31 -0500

    feat: Add execution-history-view component

---
+++ src/app/features/planning/components/execution-history-view/execution-history-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-execution-history-view',
+  imports: [],
+  templateUrl: './execution-history-view.component.html',
+  styleUrl: './execution-history-view.component.scss'
+})
+export class ExecutionHistoryViewComponent {
+
+}
