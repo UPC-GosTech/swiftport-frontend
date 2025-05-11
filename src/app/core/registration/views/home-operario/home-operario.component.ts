4bdc1245ddf406fee2d883b6d97f4011e2e48110
natalia <natalia.roman.cruz@gmail.com>
2025-05-11 15:06:38 -0500

    feat: Add home components

---
+++ src/app/core/registration/views/home-operario/home-operario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-operario',
+  imports: [],
+  templateUrl: './home-operario.component.html',
+  styleUrl: './home-operario.component.scss'
+})
+export class HomeOperarioComponent {
+
+}
