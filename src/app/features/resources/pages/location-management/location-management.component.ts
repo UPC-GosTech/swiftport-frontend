e57b3df13bd30e5408ac18fffebd9897dbb5492b
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-05-10 20:57:28 -0500

    feat: zone, location and coordinate models created

---
+++ src/app/features/resources/pages/location-management/location-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-management',
+  imports: [],
+  templateUrl: './location-management.component.html',
+  styleUrl: './location-management.component.scss'
+})
+export class LocationManagementComponent {
+
+}
