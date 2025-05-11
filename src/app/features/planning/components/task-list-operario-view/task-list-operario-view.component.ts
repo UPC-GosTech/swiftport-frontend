13cdbe88ffeed1113a0057bc713fa6297d30e816
natalia <natalia.roman.cruz@gmail.com>
2025-05-10 21:07:23 -0500

    feat: Add task-list-operario-view

---
+++ src/app/features/planning/components/task-list-operario-view/task-list-operario-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-operario-view',
+  imports: [],
+  templateUrl: './task-list-operario-view.component.html',
+  styleUrl: './task-list-operario-view.component.scss'
+})
+export class TaskListOperarioViewComponent {
+
+}
