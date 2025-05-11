27437b0ff65c5268eb199a41a9fa95d65faaabf8
natalia <natalia.roman.cruz@gmail.com>
2025-05-10 21:19:43 -0500

    chore: Call operario entity on task-list-operario-view component

--- src/app/features/planning/components/task-list-operario-view/task-list-operario-view.component.ts
+++ src/app/features/planning/components/task-list-operario-view/task-list-operario-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from '../../../resources/models/employee.entity';
 
 @Component({
   selector: 'app-task-list-operario-view',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './task-list-operario-view.component.scss'
 })
 export class TaskListOperarioViewComponent {
-
+  operario: Employee = new Employee();
 }
